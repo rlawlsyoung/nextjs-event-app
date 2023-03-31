1f78894ac957174b1026f83127e7e0b79a17599b
김진영 <jinyoung01099@gmail.com>
2023-03-31 17:57:48 +0900

    [Add] dummy data 및 사진 추가

---
+++ dummy-data.ts
@@ -0,0 +1,56 @@
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Punk Forever",
+    description: "THISISPUNK",
+    date: "2021-05-12",
+    image: "images/rock1.jpg",
+    isFeatured: false,
+  },
+  {
+    id: "e2",
+    title: "ROCK",
+    description: "ROCKNROLL",
+    date: "2021-05-30",
+    image: "images/rock2.jpg",
+    isFeatured: true,
+  },
+  {
+    id: "e3",
+    title: "We love punk rock",
+    description: "I'm married with punk rock",
+    date: "2022-04-10",
+    image: "images/rock3.jpg",
+    isFeatured: true,
+  },
+];
+
+interface DateFilterType {
+  year: number;
+  month: number;
+}
+
+export function getFeaturedEvents() {
+  return DUMMY_EVENTS.filter((event) => event.isFeatured);
+}
+
+export function getAllEvents() {
+  return DUMMY_EVENTS;
+}
+
+export function getFilteredEvents(dateFilter: DateFilterType) {
+  const { year, month } = dateFilter;
+
+  let filteredEvents = DUMMY_EVENTS.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
+
+export function getEventById(id: string) {
+  return DUMMY_EVENTS.find((event) => event.id === id);
+}

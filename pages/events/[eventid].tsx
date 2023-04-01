651973d7402a9a5ee25337346fda226bfd6dde0d
김진영 <jinyoung01099@gmail.com>
2023-04-01 16:17:11 +0900

    [Add] eventId 페이지 해당 이벤트 받는 기능 추가

--- pages/events/[eventid].tsx
+++ pages/events/[eventid].tsx
@@ -1,8 +1,24 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "@/dummy-data";
+
 const EventsDetailPage = () => {
-  return (
+  const router = useRouter();
+
+  const eventId = () => {
+    if (router.query.eventId && !Array.isArray(router.query.eventId))
+      return router.query.eventId;
+    return "";
+  };
+
+  const event = getEventById(eventId());
+
+  return event ? (
     <div>
       <h1>EventsDetail</h1>
     </div>
+  ) : (
+    <div>No event found!</div>
   );
 };
 

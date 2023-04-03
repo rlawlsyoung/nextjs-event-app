a30db460ad784d308c5421beabdc7c9614067268
김진영 <jinyoung01099@gmail.com>
2023-04-03 20:04:35 +0900

    [Add] eventsSearch 컴포넌트 디자인 구현

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -5,14 +5,19 @@ import classes from "./button.module.css";
 
 interface ButtonType {
   children: string;
-  link: string;
+  link?: string;
+  onClick?: () => void;
 }
 
-const Button: React.FC<ButtonType> = ({ children, link }) => {
-  return (
+const Button: React.FC<ButtonType> = ({ children, link, onClick }) => {
+  return link ? (
     <Link href={link} className={classes.btn}>
       {children}
     </Link>
+  ) : (
+    <button className={classes.btn} onClick={onClick}>
+      {children}
+    </button>
   );
 };
 
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,10 +1,12 @@
 import { getAllEvents } from "@/dummy-data";
 import EventList from "@/components/events/EventList";
+import EventsSearch from "@/components/events/EventsSearch";
 
 const EventsPage = () => {
   const events = getAllEvents();
   return (
     <>
+      <EventsSearch />
       <EventList items={events} />
     </>
   );

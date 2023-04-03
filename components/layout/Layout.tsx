37830575416b26372ea2b7d19bbec9bd275fbc6a
김진영 <jinyoung01099@gmail.com>
2023-04-03 18:53:17 +0900

    [Add] Header 및 레이아웃 구현

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import MainHeader from "./MainHeader";
+
+interface LayoutType {
+  children: JSX.Element;
+}
+
+const Layout: React.FC<LayoutType> = ({ children }) => {
+  return (
+    <>
+      <MainHeader />
+      {children}
+    </>
+  );
+};
+
+export default Layout;

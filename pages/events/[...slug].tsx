40b3d032163ae5a9bc03ae060f16f32e9a02c044
김진영 <jinyoung01099@gmail.com>
2023-04-15 19:22:52 +0900

    [Fix] slug 페이지 클라이언트 사이드 페칭 적용

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,61 +1,49 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
-import { getFilteredEvents } from "@/helpers/api-util";
 import EventList from "@/components/events/EventList";
-import { GetServerSideProps } from "next";
 import { EventType } from "@/helpers/api-util";
 
-interface FilterEventsPageProps {
-  hasError: boolean;
-  events: EventType[];
-}
+const FilterEventsPage = () => {
+  const [loadedEvents, setLoadedEvents] = useState<EventType[]>();
+  const router = useRouter();
 
-const FilterEventsPage: React.FC<FilterEventsPageProps> = ({
-  hasError,
-  events,
-}) => {
-  console.log(events);
+  const filterData = router.query.slug;
 
-  // const router = useRouter();
-
-  // const filterData = router.query.slug;
-
-  // if (!filterData) {
-  //   return <p className="center">Loading...</p>;
-  // }
-
-  // const filteredYear = Number(filterData![0]);
-  // const filteredMonth = Number(filterData![1]);
-
-  // const filteredEvents = getFilteredEvents({
-  //   year: filteredYear,
-  //   month: filteredMonth,
-  // });
-
-  return hasError ? (
-    <p className="center">
-      필터에 해당하는 이벤트가 없거나 필터 값이 잘못되었습니다.
-    </p>
-  ) : (
-    <div>
-      <EventList items={events} />
-    </div>
+  const { data, error } = useSWR(
+    "https://nextjs-event-app-f461b-default-rtdb.asia-southeast1.firebasedatabase.app/events.json",
+    (url) => fetch(url).then((res) => res.json())
   );
-};
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const filterData = params?.slug;
+  useEffect(() => {
+    console.log(data);
 
-  if (!filterData) {
+    if (data) {
+      const events = [];
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!loadedEvents) {
     return <p className="center">Loading...</p>;
   }
 
   const filteredYear = Number(filterData![0]);
   const filteredMonth = Number(filterData![1]);
 
-  const filteredEvents = await getFilteredEvents({
-    year: filteredYear,
-    month: filteredMonth,
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === filteredYear &&
+      eventDate.getMonth() === filteredMonth - 1
+    );
   });
 
   return isNaN(filteredYear) ||
@@ -65,22 +53,56 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     filteredMonth > 12 ||
     filteredMonth < 1 ||
     !filteredEvents ||
-    filteredEvents.length === 0
-    ? {
-        props: {
-          hasError: true,
-        },
-        // 주석 처리된건 2개의 다른 대안이다.
-        // notFound: true,
-        // redirect: {
-        //   destination: "/error",
-        // },
-      }
-    : {
-        props: {
-          events: filteredEvents,
-        },
-      };
+    filteredEvents.length === 0 ||
+    error ? (
+    <p className="center">
+      필터에 해당하는 이벤트가 없거나 필터 값이 잘못되었습니다. (혹은 오류)
+    </p>
+  ) : (
+    <div>
+      <EventList items={filteredEvents} />
+    </div>
+  );
 };
+
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   const filterData = params?.slug;
+
+//   if (!filterData) {
+//     return <p className="center">Loading...</p>;
+//   }
+
+//   const filteredYear = Number(filterData![0]);
+//   const filteredMonth = Number(filterData![1]);
+
+//   const filteredEvents = await getFilteredEvents({
+//     year: filteredYear,
+//     month: filteredMonth,
+//   });
+
+//   return isNaN(filteredYear) ||
+//     isNaN(filteredMonth) ||
+//     filteredYear > 3000 ||
+//     filteredYear < 1999 ||
+//     filteredMonth > 12 ||
+//     filteredMonth < 1 ||
+//     !filteredEvents ||
+//     filteredEvents.length === 0
+//     ? {
+//         props: {
+//           hasError: true,
+//         },
+//         // 주석 처리된건 2개의 다른 대안이다.
+//         // notFound: true,
+//         // redirect: {
+//         //   destination: "/error",
+//         // },
+//       }
+//     : {
+//         props: {
+//           events: filteredEvents,
+//         },
+//       };
+// };
 
 export default FilterEventsPage;
